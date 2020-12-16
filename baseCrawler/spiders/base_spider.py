698c73b6a21cd6de2d7360fd35f061d7766bb105
jishnug <jishnug@factweavers.com>
2020-12-16 12:10:55 +0530

    issue repoduced

--- baseCrawler/spiders/base_spider.py
+++ baseCrawler/spiders/base_spider.py
@@ -1,15 +1,35 @@
 import scrapy
-from link_extractor import LinkExtractor
-
+# from link_extractor import LinkExtractor
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+import time
 
 class BaseSpider(scrapy.Spider):
     name = "base"
+    link_extractor = LxmlLinkExtractor(allow=(), deny=(), allow_domains=(["example.com"]), deny_domains=(),
+                                       deny_extensions=None, restrict_xpaths=(), restrict_css=(), tags=('a', 'area'),
+                                       attrs=('href',), canonicalize=False, unique=True, process_value=None, strip=True)
 
     def start_requests(self):
         base_url = 'http://example.com/'
+
         yield scrapy.Request(url=base_url, callback=self.parse)
 
     def parse(self, response):
-        links = LinkExtractor(response).get_links()
+
+        links = self.link_extractor.extract_links(response)
+        links_processing_start = time.time()
         for link in links:
-            yield scrapy.Request(link.url, callback=self.parse)
+            yield {
+                'source_url': response.url,
+                'destination_url':link.url
+            }
+            yield scrapy.Request(url=link.url, callback=self.parse)
+        print("TOTAL TIME TOOK | " + str(time.time() - links_processing_start) + " | TOTAL LINKS COUNT | " + str(len(links)))
+        yield {
+            'url': response.url,
+            'download_time': response.meta['__end_time'] - response.meta['__start_time'],
+            'download_latency': response.meta['download_latency']
+        }
+
+    def parseError(self, response):
+        print('Error')
